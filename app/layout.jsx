798b778f70726e84cb4df95ff17b8baabdc2a4da
Al Jami Islam Anik <anik.islam1494@gmail.com>
2024-08-02 07:26:56 +0600

    Added home and about page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,36 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
 
+import {Josefin_Sans} from "next/font/google"
+import "@/app/_styles/globals.css"
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets:['latin'],
+  display:"swap"
+})
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template:"%s - The Wild Oasis",
+    default:"Welcome - The Wild Oasis"
+  },
   description: "Title of the app",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        
-        <main>
+      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">
           {children}
         </main>
 
+        </div>
+        
         </body>
     </html>
   );
